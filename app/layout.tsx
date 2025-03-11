4f200e3b2ca032ae27429b727bc65970b8de92bd
Sylvia Llorente <sylviall81@gmail.com>
2025-03-11 15:11:36 +0100

    adding Opengraph tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,22 @@ export const metadata: Metadata = {
     ],
   },
   manifest: "/site.webmanifest",
+  
+  openGraph: {
+    title: "Sylvia Llorente | Portfolio",
+    description: "Portfolio personal de desarrollo web",
+    url: "https://sllorentedev.vercel.app/",
+    siteName: "Sylvia Llorente Portfolio",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dsesprxhl/image/upload/v1741702164/portfolio/portfolioSN_fpf4db.jpg", // Imagen para compartir en redes
+        width: 1200,
+        height: 630,
+        alt: "Preview de Sylvia Llorente Portfolio",
+      },
+    ],
+    type: "website",
+  },
 }
 
 export default function RootLayout({
